e8496b9bd94a799d0d7c45d5d2667b8830e93ebb
parthraj27 <parthraj27@gmail.com>
2026-09-13 17:38:44 +0530

    fix: remove obsolete migration export

--- src/infrastructure/persistence/migrations/index.ts
+++ src/infrastructure/persistence/migrations/index.ts
@@ -15,5 +15,4 @@ export * from './1789063900541-migration';
 export * from './1789069921140-migration';
 export * from './1789071798928-migration';
 export * from './1789278719843-migration';
-export * from './1789298008260-migration';
 export * from './1789299782941-migration';
